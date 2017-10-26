64a09edbe6ea349fa9cff3c735fb1f0bf02a9b18
Jean-Charles <jeancharles.roger@me.com>
2017-10-26 11:33:35 +0200

    Adds duration list

--- app/src/main/java/sporttimer/minibilles/fr/sporttimer/DurationListAdapter.kt
+++ app/src/main/java/sporttimer/minibilles/fr/sporttimer/DurationListAdapter.kt
@@ -3,22 +3,20 @@ package sporttimer.minibilles.fr.sporttimer
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
 class DurationListAdapter(val durations: List<Double>) : BaseAdapter() {
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val textView = TextView(parent?.context)
+        textView.text = "Duration $position"
+        return textView
     }
 
-    override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItem(position: Int): Any = durations[position]
 
-    override fun getItemId(position: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemId(position: Int): Long = position.toLong()
 
-    override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getCount(): Int = durations.size
 
 }
